ca70369026ab1f7b0ba67da8ce84cc5f881085bf
 明辉 <406875886@qq.com>
2019-01-22 11:01:09 +0800

    更新注释

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
+// 必须引入2个 js 文件
 import React from "react";
 import ReactDOM from "react-dom";
 /**
- * 写一个 React 函数组件，要求：
+ * 题目：写一个 React 函数组件，要求：
  *    接受 name 和 age 作为 props
  *    在 div 里展示 name 和 age
  */
+
+// 继承 React.Component
 class App extends React.Component {
   constructor() {
     super();
+    // 自己的数据
     this.state = {
       work: "front end"
     };
@@ -24,4 +28,5 @@ class App extends React.Component {
 }
 
 const rootElement = document.getElementById("root");
+// 标签App就是函数，函数就是对象，标签的属性就是函数的参数（传入的数据）
 ReactDOM.render(<App name="wmh" age="18" />, rootElement);
