cccc8dd099acece0b1b84229f29d6505fc2c2cb6
 明辉 <406875886@qq.com>
2019-01-22 11:13:51 +0800

    事件绑定 this,数据更新 setState

--- src/index.js
+++ src/index.js
@@ -9,23 +9,54 @@ import ReactDOM from "react-dom";
 
 // 继承 React.Component
 class App extends React.Component {
-  constructor() {
-    super();
+  // 初始化数据
+  constructor(props) {
+    super(props);
     // 自己的数据
     this.state = {
-      work: "front end"
+      work: "front end",
+      workNumber: 1
     };
   }
+
+  add() {
+    // 改变数据，只能用setState，并且注意它是异步的。
+    this.setState({
+      workNumber: this.state.workNumber + 1
+    });
+
+    /*
+    想连续操作workNumber，用下面的写法
+    this.setState(state => {
+      console.log(state);
+      return {
+        workNumber: state.workNumber + 1
+      };
+    });
+    this.setState(state => {
+      console.log(state);
+      return {
+        workNumber: state.workNumber + 1
+      };
+    });
+    */
+  }
+
   render() {
+    // 这里事件要绑定 this， 不绑定就是 undefined，react 强制设置的
     return (
       <div className="App">
         <h1>Hello {this.props.name}</h1>
         <p>年龄：{this.props.age}</p>
         <p>工作：{this.state.work}</p>
+        <p>工龄：{this.state.workNumber}</p>
+        <button onClick={this.add.bind(this)}>工龄加1</button>
       </div>
     );
   }
 }
+
+class ss {}
 
 const rootElement = document.getElementById("root");
 // 标签App就是函数，函数就是对象，标签的属性就是函数的参数（传入的数据）
